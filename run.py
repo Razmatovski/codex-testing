4b8cacf44b7277f543685417cdef014c8e67a908
Razmatovski <127438757+Razmatovski@users.noreply.github.com>
2025-07-12 17:51:27 +0200

    Move db init call to main

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from admin_app import create_app, ensure_db_initialized
 
 app = create_app()
-ensure_db_initialized(app)
 
 if __name__ == '__main__':
+    ensure_db_initialized(app)
     app.run(debug=True)
 
